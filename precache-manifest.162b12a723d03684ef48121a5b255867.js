2fdc3282be33135360398a1837c801e7bc970fbc
ad-ahanadutta <ahanadutta555@gmail.com>
2020-10-07 20:55:16 +0530

    Updates

---
+++ precache-manifest.162b12a723d03684ef48121a5b255867.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62dacf5c67effa7f1c6acbf9eb6f64c",
+    "url": "/GO-CORONA/index.html"
+  },
+  {
+    "revision": "9cfa9d4b846571df0122",
+    "url": "/GO-CORONA/static/css/main.62008f7a.chunk.css"
+  },
+  {
+    "revision": "71f0798b27f200511f7d",
+    "url": "/GO-CORONA/static/js/2.a37be929.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/GO-CORONA/static/js/2.a37be929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfa9d4b846571df0122",
+    "url": "/GO-CORONA/static/js/main.14b70fdb.chunk.js"
+  },
+  {
+    "revision": "f54722bff479bc015470",
+    "url": "/GO-CORONA/static/js/runtime-main.aad90c8a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/GO-CORONA/static/media/image.d7265326.png"
+  }
+]);
